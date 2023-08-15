72343f6e34da6ffb0ed1dfd5eaca651c5cb42e54
ShahinTheDeveloper <inboxtoshahin@gmail.com>
2023-08-15 23:50:23 +0600

    color add

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,10 @@ export default function Home() {
     return (
         <div className="flex place-items-center">
             <h1 className="flex justify-self-center	">
-                Welcome To My &nbsp; <Link href={"/blog"}> Blog</Link>
+                Welcome To My &nbsp;
+                <Link className="text-sky-400/100" href={"/blog"}>
+                    Blog
+                </Link>
             </h1>
         </div>
     );
